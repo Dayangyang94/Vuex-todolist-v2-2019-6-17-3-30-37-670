9cb6272ffba15641abc58f6ade47c230132912e9
gaoyang <2436512082@qq.com>
2019-09-04 20:13:35 +0800

    put

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div>
+        <h1>Welcome to {{username}}!</h1>
+        <br/>
+         <div><router-link :to="'/welcome/'+username">Welcome</router-link></div>
+        <br/>
+        <div><router-link :to="{name:'counter'}">Go to Counter</router-link></div> 
+        <br/>
+        <div><router-link :to="{name: 'toDoList'}">Go to toDoList</router-link></div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "welcome",
+    props: ['username']
+}
+</script>
